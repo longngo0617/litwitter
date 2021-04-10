2925cbb6b6a25bb39c8e3c2773d1d062f2b85bea
Long Ngo <ngobi0617@gmail.com>
2021-04-10 15:23:45 +0700

    update ui follow

--- src/pages/profile/components/Follower.tsx
+++ src/pages/profile/components/Follower.tsx
@@ -25,6 +25,16 @@ export const Follower: React.FC<FollowersProps> = ({}) => {
           </Link>
         </div>
       </nav>
+      <div className="profile__wrapper">
+          <div className="empty">
+              <div className="empty--text">
+                  <span className="title">Bạn chưa theo dõi ai hết</span>
+              </div>
+              <div className="empty--text empty--info">
+                  <span className="info">Khi một người nào đó theo dõi bạn, bạn sẽ thấy họ ở đây</span>
+              </div>
+          </div>
+      </div>
     </div>
   );
 };
